2d182835cd1b49400dbbd8e42ce808b1dd24c896
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-03-23 17:54:56 +0900

    login 버튼 class를 불러오므로 login -> .login으로 수정

---
+++ login.js
@@ -0,0 +1,22 @@
+const loginButton = document.querySelector(".login");
+
+// 로그인 버튼 활성화 
+function toggleButton() {
+    // 이메일, 비밀번호 유효성 검사 및 값이 있는지 확인
+    if (emailErr.textContent === "" && passwordErr.textContent === "" &&
+        emailInput.value.trim() !== "" && passwordInput.value.trim() !== "") {
+        loginButton.disabled = false;
+        loginButton.classList.add("active");
+    } else {
+        loginButton.disabled = true;
+        loginButton.classList.remove("active");
+    }
+}
+
+// 로그인 폼 제출 이벤트
+document.querySelector(".login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (!loginButton.disabled) {
+        window.location.href = "/items.html";
+    }
+});
